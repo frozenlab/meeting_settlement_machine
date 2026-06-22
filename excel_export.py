e69562337423807d582005107e7072dd2ab7f7c1
frozenlab <dlwo90573@gmail.com>
2026-06-22 12:38:10 +0000

    modified:   excel_export.py 	new file:   "main \354\210\230\354\240\225 \354\235\264\354\240\204.txt" 	modified:   main.py

---
+++ excel_export.py
@@ -0,0 +1,42 @@
+from io import BytesIO
+import pandas as pd
+
+def create_excel(result, details, items, food_price, unit):
+    result_df = pd.DataFrame(
+        [
+            {
+                "이름": person,
+                "정산금액": amount
+            }
+            for person, amount in result.items()
+        ]
+    )
+    detail_rows = []
+
+    for person, detali_list in details.items():
+        for detail in detail_list:
+            detail_rows.append({
+                "이름" : person, "내역" : detail
+            })
+    detail_df = pd.DataFrame(detail_rows)
+
+    item_df = pd.DataFrame(
+        [
+            {
+                "항목명" : item.name,
+                "금액" : item.price,
+                "메모" : item.memo
+            }
+            for item in items
+        ]
+    )
+
+    buffer = BytesIO()
+
+    with pd.ExcelWriter(buffer, engine="openpyxl") as writer:
+        result_df.to_excel(writer, sheet_name = "정산결과", index = False)
+        detail_df.to_excel(writer, sheet_name = "추가부담내역", index = False)
+        item_df.to_excel(writer, sheet_name = "항목내역", index = False)
+        food_df.to_excel(writer, sheet_name = "설정", index = False)
+    
+    return buffer.getvalue()
